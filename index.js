0e911e3b43b9990cf019700a4462c579e56957bb
grantkee <grant.d.kee@gmail.com>
2019-09-03 23:02:21 -0500

    after importing body-parser, I can't get my post to work. console.log(req.body) returns an empty object but I can push the new object to the users array.

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
 
 const { users } = require('./state')
 
-/* BEGIN - create routes here */
+app.use(bodyParser.json())
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+app.get('/users/1', (req, res) => {
+  res.json(users[0])
+})
+app.post('/users', (req, res) => {
+  let hardCoded = {
+    "_id": 6,
+    "name": "Danny Boy",
+    "occupation": "NSA Agent",
+    "avatar": "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
+  }
+  //res.json(users[users.length - 1])
+  res.send(hardCoded)
+  console.log(req.body)
+  users.push(hardCoded)
+})
+app.put('/users/1', (req, res) => {
+  users[0].name = 'Brittney Spears'
+  res.json(users[0])
+})
+app.delete('/users/1', (req, res) => {
+  users.shift()
+  res.send('Deleted User 1')
+})
 
 
 /* END - create routes here */
 
-app.listen(port, () => 
-  console.log(`Example app listening on port ${port}!`))
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`));
