bbbdf41db1da7f3ce87e738e94156d578da36bf9
jaywiles <jay.wiles@gmail.com>
2019-09-03 20:11:07 -0500

    something

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const { users } = require('./state')
 /* BEGIN - create routes here */
 
 app.use((req, res, next) => {
-  if(req.path === "/users" && req.method === "GET") {
+  if (req.path === "/users" && req.method === "GET") {
     return res.json(users)
   }
   next();
 });
 
 app.use((req, res, next) => {
-  if(req.path === "/users/1" && req.method === "GET") {
+  if (req.path === "/users" && req.method === "GET") {
     return res.json(users/1)
   }
   next();
