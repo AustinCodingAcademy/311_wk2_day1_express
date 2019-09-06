6c1757b681071b42a03730cc14ad94b60fd22cbd
nae deshazer <naedeshazer@naes-MacBook-Pro.local>
2019-09-05 19:54:27 -0500

    2nd1stexpress Iseanee D

--- index.js
+++ index.js
@@ -1,58 +1,48 @@
-
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
-const bodyParser = require("body-parser")
-const { users } = require('./state')
-
-const { users } = require('./state')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+const bodyParser = require("body-parser");
+const { users } = require("./state");
 
 /* BEGIN - create routes here */
+let Counter = 6;
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
+app.get("/users", (req, res) => {
+	return res.json(users);
+});
 
-app.get('/users', (req, res) => {
-   return res.json(users)
- });
-
- app.get('/users/1', (req, res) => {
-  return res.json(users[0])
-})
+app.get("/users/1", (req, res) => {
+	return res.json(users[0]);
+});
 
 app.post("/users", (req, res) => {
-
-})
-
-
-
-
-app.post('/users', (req, res) => {
-  let hardCode = { 
-  "_id": 2,
-  "name": "seanDale Deshazer",
-  "occupation": "Couch Potatoe",
-  "avatar": "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
-};
-users.push(hardCode);
-res.json(users[users.length-1])
-})
-
-app.get('/users', (req, res) => {
-  return res.json(users)
+	Counter = 1 + Counter;
+	let hardCode = {
+		_id: Counter,
+		name: "seanDale Deshazer",
+		occupation: "Couch Potatoe",
+		avatar: "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
+  };
+	users.push(hardCode);
+	res.json(hardCode);
+	console.log(req.body);
 });
 
-app.put('/users/1', (req, res) => {
-  users[0].name = 'sean'
-  res.json(users[0])
-})
-
-app.delete('/users/1', (req, res) => {
-  users[0]._id = null;
-  res.send('DELETE')
+app.get("/users", (req, res) => {
+	return res.json(users);
 });
 
+app.put("/users/1", (req, res) => {
+	users[0].name = "sean";
+	res.json(users[0]);
+});
+
+app.delete("/users/1", (req, res) => {
+	users[0]._id = null;
+	res.send("DELETE");
+});
 
 /* END - create routes here */
 
-app.listen(port, () => 
-  console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
