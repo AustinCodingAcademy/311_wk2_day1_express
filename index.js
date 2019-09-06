04b7bc0874654c546c17d6dfd7ba2a21e4be014f
nae deshazer <naedeshazer@naes-MacBook-Pro.local>
2019-09-05 19:25:53 -0500

    1stExpress

--- index.js
+++ index.js
@@ -2,10 +2,54 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+const bodyParser = require("body-parser")
+const { users } = require('./state')
 
 const { users } = require('./state')
 
 /* BEGIN - create routes here */
+
+app.use(bodyParser.json())
+
+app.get('/users', (req, res) => {
+   return res.json(users)
+ });
+
+ app.get('/users/1', (req, res) => {
+  return res.json(users[0])
+})
+
+app.post("/users", (req, res) => {
+
+})
+
+
+
+
+app.post('/users', (req, res) => {
+  let hardCode = { 
+  "_id": 2,
+  "name": "seanDale Deshazer",
+  "occupation": "Couch Potatoe",
+  "avatar": "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
+};
+users.push(hardCode);
+res.json(users[users.length-1])
+})
+
+app.get('/users', (req, res) => {
+  return res.json(users)
+});
+
+app.put('/users/1', (req, res) => {
+  users[0].name = 'sean'
+  res.json(users[0])
+})
+
+app.delete('/users/1', (req, res) => {
+  users[0]._id = null;
+  res.send('DELETE')
+});
 
 
 /* END - create routes here */
