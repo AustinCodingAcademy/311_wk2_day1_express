c736085c229a594701065e84096c90ac4029e4a1
grantkee <grant.d.kee@gmail.com>
2019-09-07 11:32:29 -0500

    FINAL grant - cool assignement

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
 
 const { users } = require('./state')
+var counter = users.length
 
 app.use(bodyParser.json())
 
 app.get('/users', (req, res) => {
   res.json(users)
+  console.log(`number of users: ${counter}`)
 })
-app.get('/users/1', (req, res) => {
-  res.json(users[0])
+app.get('/users/:userId', (req, res) => {
+  let id = users.filter(x => x._id == req.params.userId);
+  res.json(id[0]);
 })
 app.post('/users', (req, res) => {
   let hardCoded = {
-    "_id": 6,
+    "_id": counter + 1,
     "name": "Danny Boy",
     "occupation": "NSA Agent",
     "avatar": "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
   }
-  //res.json(users[users.length - 1])
-  res.send(hardCoded)
-  console.log(req.body)
   users.push(hardCoded)
+  res.json(hardCoded)
+  console.log(req.body)
 })
-app.put('/users/1', (req, res) => {
-  users[0].name = 'Brittney Spears'
-  res.json(users[0])
+app.put('/users/:userId', (req, res) => {
+  let id = users.filter(x => x._id == req.params.userId);
+  id[0].name = 'Britney Spears';
+  res.json(id[0]);
 })
-app.delete('/users/1', (req, res) => {
-  users.shift()
-  res.send('Deleted User 1')
+app.delete('/users/:userId', (req, res) => {
+  let id = users.filter(x => x._id == req.params.userId);
+  let name = id[0].name;
+  id[0].isActive = false;
+  res.send(`Deleted User ${name}`);
 })
 
 
