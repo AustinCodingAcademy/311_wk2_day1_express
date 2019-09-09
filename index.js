f9544d795ef4cd5623e0fbe6a9c87cbe2ebec5ae
nae deshazer <naedeshazer@naes-MBP.attlocal.net>
2019-09-09 10:21:29 -0500

    1stExpressFinished

--- index.js
+++ index.js
@@ -13,9 +13,14 @@ app.get("/users", (req, res) => {
 });
 
 app.get("/users/1", (req, res) => {
-	return res.json(users[0]);
+  return res.json(users[0]);
+  
+
+
 });
 
+
+// part 2
 app.post("/users", (req, res) => {
 	Counter = 1 + Counter;
 	let hardCode = {
@@ -43,6 +48,27 @@ app.delete("/users/1", (req, res) => {
 	res.send("DELETE");
 });
 
+
+// part 3
+
+app.get("/users/:userId", (req, res) => {
+  users.push(hardCode);
+	res.json(users);
+	console.log(req.route);
+});
+
+
+app.put('/users/:userId', (req, res) => {
+  let id = users.filter(x => x._id == req.params.userId);
+  id[0].name = 'Britney Spears';
+  res.json(id[0]);
+})
+app.delete('/users/:userId', (req, res) => {
+  let id = users.filter(x => x._id == req.params.userId);
+  let name = id[0].name;
+  id[0].isActive = false;
+  res.send(`Deleted User ${name}`);
+})
 /* END - create routes here */
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
