4af2b3373c1dc8760d55a40e40c635fabb48da51
kjschwoerke <kjschwoerke@yahoo.com>
2019-10-28 01:58:50 -0500

    GET and POST methods completed

--- index.js
+++ index.js
@@ -5,23 +5,38 @@ const port = process.env.PORT || 4000
 
 const { users } = require('./state')
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 /* BEGIN - create routes here */
 
-//GET method get all users
-
+//GET method to get all users.
 app.get('/users', (req, res) => {
   res.json(users)
 })
 
-//Get method get single user
+//GET method to get single user's information.
 app.get('/users/:id', (req, res) => {
-   res.json(users.filter(user => user._id === parseInt(req.params.id)));
-   
+  //Create a variable to determing if a userID is found.
+  const found = users.some(user => user._id === parseInt(req.params.id));
+  //Create an if statement to return the user info if the ID is found, and an error if the ID is not found.
+  if(found) {
+    res.json(users.filter(user => user._id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({msg: `User ${req.params.id} not found`})
+  }
 });
-
+//_________________________________________
 //POST Method
 app.post('/users', (req, res) => {
-    res.json({_id: 6, name: "Bob Evans", occupation: "waiter", avatar: "Avatar"})
+    const newUser = {
+      _id: req.body._id,
+      name: req.body.name,
+      occupation: req.body.occupation,
+      avatar: req.body.avatar
+    }
+    users.push(newUser)
+    res.json(users)
+  //res.send(req.body)
 })
 
 //DELETE Method
