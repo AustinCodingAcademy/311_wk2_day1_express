4498205b9c156872ab0cf457568f6120d91b9324
kjschwoerke <kjschwoerke@yahoo.com>
2019-10-28 01:25:22 -0500

    pre-class commit

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ const { users } = require('./state')
 
 /* BEGIN - create routes here */
 
+//GET method get all users
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+//Get method get single user
+app.get('/users/:id', (req, res) => {
+   res.json(users.filter(user => user._id === parseInt(req.params.id)));
+   
+});
+
+//POST Method
+app.post('/users', (req, res) => {
+    res.json({_id: 6, name: "Bob Evans", occupation: "waiter", avatar: "Avatar"})
+})
+
+//DELETE Method
+app.delete('/users/1')
 
 /* END - create routes here */
 
