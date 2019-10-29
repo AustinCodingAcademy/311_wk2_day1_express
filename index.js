5ab85fe6aee95f908195f9519a7d5e99f26d8a48
vincentnqc <vincentnqc92@gmail.com>
2019-10-29 13:42:59 -0500

    done

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 
+
 const express = require('express')
 const app = express()
+
+const badyParser = require("body-parser")
+
 const port = process.env.PORT || 4000
 
 const { users } = require('./state')
 
-/* BEGIN - create routes here */
+app.use(badyParser.json())
 
+
+
+app.get("/users", (req, res) => {
+  res.json(users)
+})
+
+app.get("/users/:userId", (req, res) => {
+  console.log(req.params)
+
+  res.json(users[req.params.userId -1])
+})
+
+let newUser = {
+  "name":"Chad"
+}
+
+app.post("/users", (req, res) => {
+  users.push(req.body)
+  console.log(req.body)
+  users[users.length-1].id = users.length
+  res.json(users[users.length -1])
+})
+
+
+
+app.put("/users/:userId", (req, res) => {
+  users[req.params.userId -1].name = "Stacey"
+  res.json(users[req.params.userId -1])
+})
+
+app.delete("/users/:userId", (req, res) => {
+  // users.shift()
+  users[req.params.userId -1].isActive = false
+  res.send("Deleted")
+})
 
 /* END - create routes here */
 
