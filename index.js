f8782819f9ef7ba0f3f70f168f00e67274a33528
galvanitic <develop@rjgalvan.com>
2019-10-29 09:44:51 -0500

    Completed get and post

--- index.js
+++ index.js
@@ -6,7 +6,29 @@ const port = process.env.PORT || 4000
 const { users } = require('./state')
 
 /* BEGIN - create routes here */
+app.get('/users/:id', (req, res) => {
+  console.log(req.params.id);
+  res.json(users.filter(users => users._id === parseInt(req.params.id)));
+})
 
+// Create a user.
+//  Body parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.post('/users', (req, res) => {
+  const newUser = {
+    _id: req.body._id,
+    name: req.body.name,
+    occupation: req.body.occupation,
+    avatar: req.body.avatar
+  }
+
+  if (!newUser.name){
+    return res.status(400).json({ msg: 'Please include a name.'});
+  }
+  users.push(newUser);
+  res.json(users);
+})
 
 /* END - create routes here */
 
