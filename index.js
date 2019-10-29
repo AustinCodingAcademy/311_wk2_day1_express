858e2b328ebd111264461e5adba7162432c12586
emphillips <matt-phillips@live.com>
2019-10-29 10:30:35 -0500

    Wk2 Day1 Matt Phillips

--- index.js
+++ index.js
@@ -5,9 +5,27 @@ const port = process.env.PORT || 4000
 
 const { users } = require('./state')
 
+let newUser = {
+  "_id": 6,
+  "name": "Sterling Archer",
+  "occupation": "Agent",
+  // "avatar": "./scott.png"
+}
+
 /* BEGIN - create routes here */
+app.get('/users',(req,res) => {
+res.json(users);
+})
 
+app.get('/users/1', (req, res) => {
+  res.json(users[0]);
+})
 
+app.post ('/users', (req, res) => {
+  users.push(newUser)
+  res.json(users[users.length - 1]);
+
+})
 /* END - create routes here */
 
 app.listen(port, () => 
