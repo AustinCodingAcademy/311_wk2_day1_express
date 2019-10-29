31747dacdfb6f95bc0eeb7874ca8b45bdde26501
Casey Calkins <c.angelcalkins@outlook.com>
2019-10-28 20:54:31 -0500

    Casey Calkins - Almost finished

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 4000
 
 const { users } = require('./state')
 
-/* BEGIN - create routes here */
+app.use(bodyParser.json());
 
+/* BEGIN - create routes here */
+app.get('/users', (req, res) => {
+  res.json(users);
+})
+
+/* Gets user with ids */
+app.get('/users/:id', (req, res) => {
+  let id = users.filter(x => x._id == req.params.id);
+  res.json(id[0])
+})
+
+/* Sends a post request with user data */
+app.post('/users', (req, res) => {
+  users.push({
+    "_id": 6,
+    "name": "John Smith",
+    "occupation": "Laborer",
+    "avatar": "https://www.sideshow.com/wp/wp-content/uploads/2017/10/a-nightmare-on-elm-street-freddy-krueger-premium-format-feature-300366-740x448.jpg"
+  })
+})
+
+app.put('/users/1', (req, res) => {
+  users.push({
+    "name": "Testing names"
+  })
+})
+
+// Gets users
 
 /* END - create routes here */
 
