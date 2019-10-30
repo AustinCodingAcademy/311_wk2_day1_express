5619017090c5c75e50163db2047f280d00d9f554
Alex Munoz <anmunoz24@gmail.com>
2019-10-29 17:50:20 -0700

    part 3 working on

--- index.js
+++ index.js
@@ -1,11 +1,70 @@
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
 
 const { users } = require('./state')
 
+var counter = users.length+1
+
 /* BEGIN - create routes here */
+app.use(bodyParser.json ())
+
+app.get('/users', (req, res) => {
+  res.json(users);
+})
+
+app.get('/users/1', (req, res) => {
+  res.json(users[0]);
+})
+
+let newUser = {
+  "_id": 6,
+  "name": "Alex Munoz",
+  "occupation": "webDev",
+  "avatar": "https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg"
+}; 
+
+app.post('/users', (req, res) => {
+  users.push(req.body); 
+  let lastUser = users[users.length -1]
+  lastUser._id = counter
+  counter++
+  console.log(req.body)
+  res.json(lastUser)
+})
+
+app.get('/users', (req, res) => {
+  res.json(users);
+})
+
+app.put('/users/1', (req, res) => {
+users[0].name = 'Aaron'
+res.json(users[0]);
+})
+
+app.delete('/users/1', (req, res) => {
+  users.shift()
+  res.send('User Deleted')
+  // res.json(users)
+})
+
+app.get('/users/:userId', (req, res) => {
+  // let ifFound = null;
+  let userIndex = parseInt(req.params.userId) - 1;
+  
+  for (let i = 0; i < users.length; i++) {
+    if (users._id === parseInt(req.params.userId))  {
+      res.json(users[userIndex])
+    }   
+  }
+})
+
+
+
+
+
 
 
 /* END - create routes here */
