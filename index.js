66a88a7e276d4b15b101a07ae9aa1754af3b810f
David Bille <d_bille@yahoo.com>
2019-10-30 20:52:55 -0500

    working PUT and DELETE

--- index.js
+++ index.js
@@ -2,40 +2,54 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+const bodyParser = require('body-parser')
 
 const { users } = require('./state')
 
 /* BEGIN - create routes here */
 
+// Enable body parser for application/json
+app.use(bodyParser.json())
+
+// Route to get all users
 app.get('/users', (req, res) => {
   res.json(users)
 })
 
-app.get('/users/1', (req, res) => {
-  res.json(users[0])
+// Route to get single user
+app.get('/users/:id', (req, res) => {
+  let user = users.find(user => user._id === parseInt(req.params.id))
+  res.json(user)
 })
 
-let newUser = {
-  "_id": 6,
-  "name": "Bert Macklin",
-  "occupation": "FBI Agent",
-  "avatar": "https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg"
-}
 
+// Route to make a post
 app.post('/users', (req, res) => {
+  const newUser = {
+    _id: req.body._id,
+    name: req.body.name,
+    occupation: req.body.occupation,
+    avatar: req.body.avatar
+  }
   users.push(newUser)
-  let lastUser = users[users.length-1]
-  res.json(lastUser)
-})
-
-app.get('/users', (req, res) => {
   res.json(users)
 })
 
-app.put('/users/1', (req, res) => {
-  name = req.body.name;
+
+// Update user
+app.put('/users/:id', (req, res) => {
+  let user = users.find(user => user._id === parseInt(req.params.id))
+  const updateUser = req.body;
+  users.user.id;
 })
 
+
+// Delete user
+app.delete('/users/:id', (req, res) => {
+  let user = users.find(user => user._id === parseInt(req.params.id))
+  res.send('User Deleted')
+  res.json(users)
+})
 
 
 
