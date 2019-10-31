9b5f5a5a1c886fa563979e0a417ccfaa0265586a
JustinBarsalou <justin.barsalou@g.austincc.edu>
2019-10-30 19:18:05 -0500

    added express practice files

--- index.js
+++ index.js
@@ -7,6 +7,35 @@ const { users } = require('./state')
 
 /* BEGIN - create routes here */
 
+let newUser = {
+  id: 6,
+  name: 'PeePee',
+  occupation: 'Federal Boob Inspector',
+  avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTShirBiF989tESHmrZ-89FjsJrlTsMJMjRt864q_nHA0Cd3KDp'
+}
+
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+
+app.get('/users/1', (req, res) => {
+  res.json(users[0])
+
+})
+
+app.post('/users', function (req, res) {
+  users.push(newUser)
+  res.json(users[users.length - 1]);
+
+})
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+
 
 /* END - create routes here */
 
