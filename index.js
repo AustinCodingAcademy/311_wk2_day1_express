4a37f73ccd7be328eede21f245fc87f35baa9108
David Bille <d_bille@yahoo.com>
2019-11-02 14:51:59 -0500

    practice adding routes to express day 1 homework

--- index.js
+++ index.js
@@ -1,81 +1,20 @@
+const express = require('express');
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+//set up express app
+const app = express();
+
+// Enable body parser for application/json
 const bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
-const { users } = require('./state')
 
 /* BEGIN - create routes here */
 
-// Enable body parser for application/json
-app.use(bodyParser.json())
-
-// Route to get all users
-app.get('/users', (req, res) => {
-  res.json(users)
-})
-
-// Route to get single user
-app.get('/users/:id', (req, res) => {
-  let user = users.find(user => user._id === parseInt(req.params.id))
-  res.json(user)
-})
-
-// Route to make a post
-app.post('/users', (req, res) => {
-  const newUser = {
-    _id: req.body._id,
-    name: req.body.name,
-    occupation: req.body.occupation,
-    avatar: req.body.avatar
-  }
-  users.push(newUser)
-  res.json(users)
-})
-
-
-// Update user
-app.put('/users/:id', (req, res) => {
-  
-  const found = users.some(user => user._id === parseInt(req.params.id));
-  
-  if (found) {
-    const updateUser = req.body;
-    users.forEach(user => {
-      if (user._id === parseInt(req.params.id)) {
-        user.name = updateUser.name ? updateUser.name : user.name;
-        user.occupation = updateUser.occupation ? updateUser.occupation : user.occupation;
-        user.avatar = updateUser.avatar ? updateUser.avatar : user.avatar;
-
-        res.json({ msg: "User updated successfully", user });
-      }
-    });
-  } else {
-    res.status(400).json({ msg: `No User with id of ${req.params.id}` });
-  }
-})
-
-
-// Delete user
-app.delete('/users/:id', (req, res) => {
-  const found = users.some(user => user._id === parseInt(req.params.id));
-
-  if (found){
-    let user = users.find(user => user._id === parseInt(req.params.id))
-    console.log(user)
-    user.isActive = false;
-    res.json({
-      Message: 'User Deleted',
-      user
-    })
-  }
-})
-
-
-
+const routes = require('./routes/users')
+app.use(routes)
 
 /* END - create routes here */
+const port = process.env.PORT || 4000;
 
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`))
---
+++ routes/users.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const router = express.Router()
+
+const { users } = require('../state')
+
+
+// Route to get all users
+router.get('/users', (req, res) => {
+  res.json(users)
+})
+
+// Route to get single user
+router.get('/users/:id', (req, res) => {
+  let user = users.find(user => user._id === parseInt(req.params.id))
+  res.json(user)
+})
+
+// Route to make a post
+router.post('/users', (req, res) => {
+  const newUser = {
+    _id: req.body._id,
+    name: req.body.name,
+    occupation: req.body.occupation,
+    avatar: req.body.avatar
+  }
+  users.push(newUser)
+  res.json(users)
+})
+
+
+// Update user
+router.put('/users/:id', (req, res) => {
+  
+  const found = users.some(user => user._id === parseInt(req.params.id));
+  
+  if (found) {
+    const updateUser = req.body;
+    users.forEach(user => {
+      if (user._id === parseInt(req.params.id)) {
+        user.name = updateUser.name ? updateUser.name : user.name;
+        user.occupation = updateUser.occupation ? updateUser.occupation : user.occupation;
+        user.avatar = updateUser.avatar ? updateUser.avatar : user.avatar;
+
+        res.json({ msg: "User updated successfully", user });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No User with id of ${req.params.id}` });
+  }
+})
+
+
+// Delete user
+router.delete('/users/:id', (req, res) => {
+  const found = users.some(user => user._id === parseInt(req.params.id));
+
+  if (found){
+    let user = users.find(user => user._id === parseInt(req.params.id))
+    console.log(user)
+    user.isActive = false;
+    res.json({
+      Message: 'User Deleted',
+      user
+    })
+  }
+})
+
+module.exports = router;
