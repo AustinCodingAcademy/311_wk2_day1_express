a69a6df06c60b25756e832c5cdd1c4bfcfa23a3c
galvanitic <develop@rjgalvan.com>
2019-11-03 21:10:59 -0600

    Finished first server.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
 
 const { users } = require('./state')
 
@@ -15,9 +15,11 @@ app.get('/users/:id', (req, res) => {
 //  Body parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
+
+
 app.post('/users', (req, res) => {
   const newUser = {
-    _id: req.body._id,
+    _id: users.length + 1,
     name: req.body.name,
     occupation: req.body.occupation,
     avatar: req.body.avatar
@@ -28,6 +30,42 @@ app.post('/users', (req, res) => {
   }
   users.push(newUser);
   res.json(users);
+})
+
+// Update User
+app.put('/users/:id', (req, res) => {
+  const found = users.some(users => users._id === parseInt(req.params.id));
+
+  if (found) {
+    const updUser = req.body;
+    users.forEach(user => {
+      if(user._id === parseInt(req.params.id)){
+        user.name = updUser.name ? updUser.name : users.name;
+        user.occupation = updUser.occupation ? updUser.occupation : users.occupation;
+        user.avatar = updUser.avatar ? updUser.avatar : users.avatar;
+
+        res.json({msg: 'User updated', user})
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}`})
+  }
+});
+
+// User DELETE
+app.delete('/users/:id', (req, res) => {
+  // const found = users.some(user => user._id === parseInt(req.params.id));
+  let usr = users.find(user => user._id === parseInt(req.params.id));
+
+  if (usr) {
+    usr.isActive = false;
+    res.json({
+      msg: 'User deleted',
+      users: users
+    });
+  }else {
+    res.status(400).json({msg: `No member with the id of ${req.params.id}`});
+  }
 })
 
 /* END - create routes here */
