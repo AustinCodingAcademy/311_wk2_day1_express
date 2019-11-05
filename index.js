da10807b86f775a6c4882ce466b1dfe37583fc8a
JustinBarsalou <justin.barsalou@g.austincc.edu>
2019-11-04 18:01:57 -0600

    finished express

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
 const express = require('express')
 const app = express()
+var bodyparser = require('body-parser')
+app.use(bodyparser.json());
 const port = process.env.PORT || 4000
-
 const { users } = require('./state')
+var counter = users.length+1;
 
 /* BEGIN - create routes here */
 
@@ -20,19 +22,52 @@ app.get('/users', (req, res) => {
 })
 
 
-app.get('/users/1', (req, res) => {
-  res.json(users[0])
+app.get('/users/:userId', (req, res) => {
 
-})
-
-app.post('/users', function (req, res) {
-  users.push(newUser)
-  res.json(users[users.length - 1]);
+  let user = users.find(user => {
+    return user._id == req.params.userId
+  })
+  console.log(user)
+  res.json(user)
+  
 
 })
 
 app.get('/users', (req, res) => {
   res.json(users)
+})
+
+app.post('/users', function (req, res) {
+  req.body._id = counter
+  counter++
+  users.push(req.body)
+  console.log(req.body)
+  res.json(users[users.length - 1]);
+
+})
+
+app.put('/users/:userId', function (req, res) {
+    // app.set('name', 'Justin Barsalou')
+    
+    let index = users.findIndex(user => {
+      return user._id == req.params.userId
+    })
+    users[index].name = 'Justin Barsalou'
+    res.json(users[index])
+    
+})
+
+
+app.delete('/users/:userId', function (req, res) {
+  // users.shift()
+  // res.json(users)
+
+  let delIndex = users.findIndex(user => {
+    return user._id == req.params.userId
+  })
+  users.splice(delIndex, 1)
+  res.json(users)
+
 })
 
 
