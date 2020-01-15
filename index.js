ac363ef5a5b196a75b4110a8c1ca91dc7a475158
pat-white-code <pat.k.white@gmail.com>
2020-01-15 16:22:06 -0600

    Part 3 get/users/:userId done

--- index.js
+++ index.js
@@ -11,24 +11,19 @@ let idCounter = users.length;
 app.use(express.json());
 app.use(express.urlencoded({extend: false}));
 /* BEGIN - create routes here */
-//* GET /users
-// * Give your server the ability to respond to a GET request with a path "/users" and return the users array from state.js
-// * Ex. `res.json(users)`
+
 
 app.get('/users', (request, response)=>{
   response.json(users)
 })
 
-// * POST /users
-// * Give your server the ability to respond to a POST request with a path "/users" and add a hard coded user object to the users array from state.js. Use `res.json()` to send the last user in the array (should be the new one) back to the client.
-// * If you do another GET request you should see this added
-// * You will need to create the hard coded user mentioned above
-// const newUser = {
-//   _id: 6,
-//   name: "The Smoking Man",
-//   occupation: "Ominously Smoking",
-//   avatar: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2F3%2F34%2FThe_Smoking_Man_%2528X-Files%2529.jpg%2F220px-The_Smoking_Man_%2528X-Files%2529.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCigarette_Smoking_Man&tbnid=mzgu5j95i60ACM&vet=12ahUKEwiY_qPmy4TnAhWDkq0KHSFHC7AQMygAegUIARDzAQ..i&docid=OFGXqdVn3yqktM&w=220&h=293&q=the%20smoking%20man%20x%20files&ved=2ahUKEwiY_qPmy4TnAhWDkq0KHSFHC7AQMygAegUIARDzAQ"
-//   }
+app.get('/users/:id', (request, response)=> {
+  const id = request.params.id;
+  const requestedUser = users.filter(user => user._id === parseInt(id))
+  console.log(id);
+  response.send(requestedUser);
+})
+
 
   app.post('/users', (request, response)=> {
     // users.push(newUser)
