8a92a66c4db6fd5b3e054d8fce04466b8c1eb107
Rob W <robpwatkins@gmail.com>
2020-01-15 14:42:23 -0600

    started

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
 const { users } = require('./state')
 
-/* BEGIN - create routes here */
+app.get('/users', (request, response) => {
+  return response.json(users);
+})
 
+app.get('/users/1', (request, response) => {
+  return response.json(users[0]);
+})
+
+app.post('/users', (request, response) => {
+  // Hmmmm
+  request.body = 'heyoo';
+  console.log(users)
+})
 
 /* END - create routes here */
 
