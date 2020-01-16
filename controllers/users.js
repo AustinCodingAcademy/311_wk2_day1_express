35ec86f06478689b22d07708acff4ea5e84c6034
Pace Davis <pacemdavis@gmail.com>
2020-01-16 17:43:54 -0600

    almost there

---
+++ controllers/users.js
@@ -0,0 +1,69 @@
+const { users }  = require('../state')
+let counter = users.length
+
+const users_get_all = (req, res) => {
+  return res.json(users)
+ }
+
+const users_by_id = (req, res) => {
+  const found =  users.some(users => users._id === parseInt(req.params._id));
+
+  if(found) {
+  res.json(users.filter(users => users._id === parseInt(req.params._id)))
+  } else {
+    res.status(400).json({msg: `No members with the id of ${req.params._id}`})
+  } 
+ }
+
+const post_user = (req, res) => {
+  const newUser = {
+    _id: counter += 1,
+    name: req.body.name,
+    occupation: req.body.occupation,
+    avatar: req.body.avatar,
+  }
+  if(!newUser.name || !newUser.occupation) {
+    res.status(400).json({msg: 'Please include a name and occupation'});
+  }
+  users.push(newUser);
+  res.json(users)
+}
+
+const update_user = (req, res) => {
+  const found = users.some(users => users._id === parseInt(req.params._id));
+
+  if(found) {
+    const updUser = req.body;
+    users.forEach(users => {
+      if(users._id === parseInt(req.params._id)) {
+        users.name = updUser.name ? updUser.name : users.name;
+        users.occupation = updUser.occupation ? updUser.occupation : users.occupation;
+        users.avatar = updUser.avatar ? updUser.avatar : users.avatar;
+
+        res.json({msg: 'Member updated', users})
+      }
+    });
+  } else {
+    res.status(400).json({msg: `No member with the id of ${req.params._id}`});
+  }
+ }
+
+ const delete_user = (req, res) => {
+  const found =  users.some(users => users._id === parseInt(req.params._id));
+
+  if(found) {
+  res.json({msg: 'deleted',
+   users: users.filter(users => users._id !== parseInt(req.params._id))
+  });
+  } else {
+    res.status(400).json({msg: `No members with the id of ${req.params._id}`})
+  } 
+ }
+
+ module.exports = {
+  users_get_all,
+  users_by_id,
+  post_user,
+  update_user,
+  delete_user
+}
--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
-const { users } = require('./state')
 
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use('/users', require('./routes/api/users'))
 /* BEGIN - create routes here */
+
 
 
 /* END - create routes here */
