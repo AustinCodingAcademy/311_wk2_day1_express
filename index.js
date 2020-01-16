a2cb53c9af6dabbb1457533967f4d90d28600361
Rob W <robpwatkins@gmail.com>
2020-01-16 11:52:28 -0600

    whew.

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
 
 const { users } = require('./state')
 
-app.get('/users', (request, response) => {
-  return response.json(users);
-})
+// app.get('/users', (request, response) => {
+//   return response.json(users);
+// })
 
-app.get('/users/1', (request, response) => {
-  return response.json(users[0]);
-})
+// app.get('/users/1', (request, response) => {
+//   return response.json(users[0]);
+// })
 
-app.post('/users', (request, response) => {
-  request = {
-    "_id": 6,
-    "name": "Tiny Dinky Daffy",
-    "occupation": "Baby",
-    "avatar": "https://thetinydinkster.jpg"
-  }
-  users.push(request);
-  return response.json(users);
-})
+// app.post('/users', (request, response) => {
+//   request = {
+//     "_id": 6,
+//     "name": "Tiny Dinky Daffy",
+//     "occupation": "Baby",
+//     "avatar": "https://thetinydinkster.jpg"
+//   }
+//   users.push(request);
+//   return response.json(users);
+// })
 
-app.put('/users/1', (request, response) => {
-  request = 'Bart Harley Jarvis';
-  users[0].name = request;
-  return response.json(users[0]);
-})
+// app.put('/users/1', (request, response) => {
+//   request = 'Bart Harley Jarvis';
+//   users[0].name = request;
+//   return response.json(users[0]);
+// })
 
-app.delete('/users/1', (request, response) => {
-  request.body = 1;
-  users.shift();
-  response.send(`User ${request.body} deleted`);
+// app.delete('/users/1', (request, response) => {
+//   request.body = 1;
+//   users.shift();
+//   response.send(`User ${request.body} deleted`);
+// })
+
+// app.use(bodyParser.json());
+
+// app.post('/users', (request, response) => {
+//   request.body._id = users.length;
+//   request.body._id ++;
+//   users.push(request.body);
+//   return response.json(users[users.length - 1]);
+// })
+
+app.get('/users/:userId', (request, response) => {
+  let userId = request.params.userId;
+  return response.json(users.filter(x => x._id === parseInt(userId)));
 })
 
 /* END - create routes here */
