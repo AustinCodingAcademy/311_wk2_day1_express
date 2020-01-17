10242c5be7a7ac034b34c3077c2ad83558477d19
RiinaMiichelle <sarina.m.colosimo@gmail.com>
2020-01-16 20:49:01 -0600

    worked on express hw

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
 
@@ -16,30 +17,46 @@ app.get('/users/1',(req, res)  => {
   return res.json(users[0])
 })
 
-// app.post('/users', (req, res) => {
-//   const usersLengthBefore = users.length;
-//   users.push({
-//     "_id": usersLengthBefore + 1,
-//     "name": `Hardcoded - user ${usersLengthBefore + 1}`,
-//     "occupation": `Hardcoded - user ${usersLengthBefore + 1}`,
-//     "avatar": "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
-//   })
-//   return res.json(users[users.length -1]);
-// })
-
 app.post('/users', (req, res) => {
+  const usersLengthBefore = users.length;
   users.push({
-    "_id": 6,
-    "name": "Sarina Colosimo",
-    "occupation": "Ads Reviewer",
+    "_id": usersLengthBefore + 1,
+    "name": `Hardcoded - user ${usersLengthBefore + 1}`,
+    "occupation": `Hardcoded - user ${usersLengthBefore + 1}`,
     "avatar": "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
   })
-  return res.json(users[5]);
+  return res.json(users[users.length -1]);
 })
 
 app.put('/users/1', (req, res) => {
   users[0].occupation = "A new occupation!";
   return res.json(users[0]);
+})
+
+app.delete('/users/1', (req, res) => {
+  users.pop()
+  return res.send('deleted');
+})
+
+
+//part 3 # 1
+app.get('/users/:_id', (req, res) => {
+  return res.json(users.filter(users => users._id === parseInt(req.params._id)));
+})
+
+// using variables as id
+// app.get("/users/:userId", (req, res) => {
+//   let theIdImLookingFor = req.params.userId;
+//   console.log(theIdImLookingFor);
+// }
+
+app.put('/users/:userId', (req, res) => {
+  let userId = req.params.userId;
+  let currentUser = users.find(function (user) {
+    return user._id == userId;
+  })
+  currentUser.occupation = "Dog Walker!";
+  return res.json(currentUser);
 })
 
 
