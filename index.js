5cbf7f960f5b473d32af14ebfa8c0d69f49e56c8
dgasch512 <gaschdav208@gmail.com>
2020-01-17 21:50:45 -0600

    Completed file with all requests functioning

--- index.js
+++ index.js
@@ -1,43 +1,81 @@
+// require express
+const express = require('express');
+const bodyParser = require('body-parser');
+// const moment = require('moment');
+const app = express();
+// das port
+const port = process.env.PORT || 4000;
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+// connect to state.js, user array
+const { users } = require('./state');
 
-const { users } = require('./state')
+// counter for id creation
+const counter = function() {
+  let id = users.length;
+  id++;
+  return id;
+}
+
+// middleware - response to jsn and nodemon
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 /* BEGIN - create routes here */
 app.use('/users', function (req, res, next) {
-  console.log('Time: ', Date.now())
+  console.log('Oops I Did It Again');
+  // console.log(
+  //   `${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`
+  // );
   next()
 })
 
-app.get('/users', function (req, res) {
-  return res.json(users)
-})
+app.get('/users', (req, res) => res.json(users));
 
 app.get('/users/:id(\\d+)', function(req, res) {
-  // res = res.json(req.params.id);
+  // res.json(users.filter(user => user._id === parseInt(req.params.id)));
   let usrID = parseInt(req.params.id);
   let user = users.find(arrObj => arrObj._id === usrID);
-  // let user = users.find(usr => usr.id === res);
-  // let id = req;
-  // let user = users.find((usr) => usr._id === id);
+
   console.log(users);
   res.json(user);
          
 })
 
 app.post('/users', function(req, res) {
-  // let _id = req.body.id;
-  let name = req.body.name;
-  let occupation = req.body.occupation;
-  let avatar = req.body.avatar;
-
-  res.json(users.lastIndexOf)
+  // let newUser = req.body;
+  const newUser = {
+    _id: counter(),
+    name: req.body.name,
+    occupation: req.body.occupation,
+    avatar: req.body.avatar,
+    status: 'active'
+  }
+  
+  users.push(newUser);
+  console.log(newUser);
+  res.json(users);
+  
 })
 
+app.put('/users/:id', function(req, res) {
+  let usrID = parseInt(req.params.id);
+  let user = users.find(arrObj => arrObj._id === usrID);
+  user.occupation = req.body.occupation;
+
+  console.log(user);
+  res.json(user);
+})
+
+app.delete('/users/:id', function(req, res) {
+  let usrID = parseInt(req.params.id);
+  let user = users.find(arrObj => arrObj._id === usrID);
+  user.isActive = 'false';
+
+  res.json({msg: 'User Deleted', users: users.filter(user => user._id !== parseInt(req.params.id))});
 
 
+  res.send('User Deleted');
+})
 
 
 /* END - create routes here */
