3fcbad0d1190711d985f08616bac8ba21d7ba6da
carl-larson <clarson08@gmail.com>
2020-03-05 18:26:24 -0600

    just seeing if all my git repos are up to date

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+const express = require('express');
+const bodyParser = require('body-parser');
+const { users } = require('./state');
 
-const { users } = require('./state')
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+
+let counter = users.length;
 
 /* BEGIN - create routes here */
 app.get('/users', (req, res) => {
@@ -16,29 +21,36 @@ app.get('/users/:id', (req, res) =>{
 })
 
 app.post('/users',(req, res) =>{
-  const newUser = {
-    '_id': 6,
-    'name': 'Rick Sanchez',
-    'occupation': 'Chemist',
-    'avatar': "https://www.fillmurray.com/200/300"
+  console.log(req.body);
+  // counter = counter + 1;
+  let newUser = {
+    ...req.body,
+    _id: counter+1,
   }
   users.push(newUser);
-  res.json(users[users.length - 1]);
+  console.log(users);
+  res.json(users[counter - 1]);
 })
 
-app.put('/users/1', (req, res)=>{
+app.put('/users/:userid', (req, res)=>{
   // users[0].name = 'Tim';
-  let updateUser = {
-    ...users[0],
-    name: 'Jimothy'
-  }
-  res.json(updateUser);
+  const userId = req.params.userid;
+  // let updateUser = {
+  //   ...users[userId-1],
+  console.log(users[userId -1]);
+  // }
+  // users[userId -1] = {
+  //   ...users[userId-1],
+  //   ...req.body
+  // }
+  res.json(users);
 })
 
-app.delete('/users/1', (req, res)=>{
-  let updateUsers = users.shift();
-  console.log(updateUsers)
-  res.json('deleted');
+app.delete('/users/:userid', (req, res)=>{
+  let userId = req.params.userid;
+  users.splice(userId - 1, 1);
+  console.log(users)
+  res.json(users);
 })
 
 /* END - create routes here */
