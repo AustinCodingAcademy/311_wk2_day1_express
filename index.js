f3a9f63316c903c0740bfe979f8a9bd0eb7e35a9
aaron-kane-w <aaron.kane.w@gmail.com>
2020-06-03 17:49:22 -0500

    working on assignment

--- index.js
+++ index.js
@@ -7,6 +7,32 @@ const { users } = require('./state')
 
 /* BEGIN - create routes here */
 
+// Get entire array
+app.get('/users', (req, res) => res.json(users))
+
+// Get single element from an array* work on this
+
+app.get('/users/:_id', (req, res) => {
+  res.json(users.find(user => {
+    return +req.params._id === user._id
+  }))
+})
+
+// create element in array (which requires body-parser middleware)
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.post('/users', (req, res) => {
+  const newUser = {
+    _id: req.body._id,
+    name: req.body.name,
+    occupation: req.body.occupation,
+    avatar: req.body.avatar
+  }
+  
+  users.push(newUser)
+  res.json(newUser)
+})
 
 /* END - create routes here */
 
