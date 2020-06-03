372d1e8bacb76b4c8189fbf70dae27ac771e159f
peter gilkey <peter_gilkey@yahoo.com>
2020-06-03 15:18:17 -0500

    Express server pt 1 complete

--- index.js
+++ index.js
@@ -7,6 +7,52 @@ const { users } = require('./state')
 
 /* BEGIN - create routes here */
 
+app.get('/', (req, res) => {
+  res.send("Hello World");
+})
+
+// Get request to return users array
+//
+app.get("/users", (req, res) => {
+  res.json(users);
+})
+
+// Get request to return user by ID number
+//
+app.get("/users/:id", (req, res) => {
+  let id = req.params.id -1;
+  res.json(users[id]);
+})
+
+// Post request to add new user to users array
+//
+app.post("/users", (req, res) => {
+  let newUser = {
+    "id": 6,
+    "name": "Sean Evans",
+    "occupation": "Wing-tester",
+    "avatar": "Photo-of-Sean-Evans"
+  }
+  users.push(newUser)
+  res.json(users);
+})
+
+// Put request to modify user1 of array of users
+//
+app.put("/users/:id", (req, res) => {
+  let id = req.params.id -1;
+  users[id].name = "Dale Doback";
+  users[id].occupation = "Sailor";
+  res.json(users[id]);
+})
+
+// Delete request to remove user from array
+//
+app.delete("/users/:id", (req, res) => {
+  let id = req.params.id -1;
+  users.splice(id, 1);
+  res.send('deleted');
+})
 
 /* END - create routes here */
 
