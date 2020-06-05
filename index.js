1f27d81ae467713c3d2d75faa8f9d195df93247d
CodingGiraffe <adamthomas03@gmail.com>
2020-06-05 10:03:57 -0500

    finished homework

--- index.js
+++ index.js
@@ -13,83 +13,88 @@ app.use(express.urlencoded({extended: false}))
 /* BEGIN - create routes here */
 
 // GET Method to get all users 
-//get all users
-// app.get('/users', (req, res) => {
+app.get('/users', (req, res) => {
 
-//   res.json(users)
-// })
+  res.json(users)
+})
 
-//get first member 
-// app.get('/users/1', (req, res) => {
+//GET - First Member 
+app.get('/users/1', (req, res) => {
   
-//   res.json(users[0])
-// })
+  res.json(users[0])
+})
 
-//add a new user to array
-// app.post('/users', (req, res) => {
-// users.push({"_id": "6","name":"Dana Scully","occupation":"FBI Agent","avatar":"https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg"})
-// res.json(users)
-// })
+//POST - Add New User
+app.post('/users', (req, res) => {
+users.push({"_id": "6","name":"Dana Scully","occupation":"FBI Agent","avatar":"https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg"})
+res.json(users)
+})
  
-// Update User
-// app.put('/users', (req, res) => {
-//   users.name = req.body.name
+// PUT - Update User
+app.put('/users', (req, res) => {
+  const newUser = {
+    _id: req.body._id,
+    name: req.body.name,
+    occupation: req.body.occupation,
+    avatar: req.body.avatar 
+  }
 
-//   res.json(users)
-// })
+  users.push(newUser)
+  res.json(users)
 
-//Delete User
-// app.delete('/users/1', (req, res) => {
-    
-//   res.send("Deleted")
-// })
+//  DELETE - Delete User
+app.delete('/users/1', (req, res) => {
+    let id = req.params.id -1
+    users.splice(id, 1)
+    users[id].isActive = "false"
+    res.send('deleted')
+})
 
 //BODY-PARSER MODULE
-// app.post('/users', (req, res) => {
-//   const newUser = {
-//     _id: req.body._id,
-//     name: req.body.name,
-//     occupation: req.body.occupation,
-//     avatar: req.body.avatar 
-//   }
 
-//   users.push(newUser)
-//   res.json(users)
+app.use(bodyParser.json())
 
-//Get User - GET PV
-app.get('/users/:userId', (req, res) => {
-  let getId = users.filter(user => user._id === parseInt(req.params.userId))
-  res.json(getId)
+let counter = 0
+
+app.post('/users', (req, res) => {
+  req.body._id = counter++
+  users.push(req.body)
+  res.json(users[counter])
 })
+//Get User - GET PV
+// app.get('/users/:userId', (req, res) => {
+//   let getId = users.filter(user => user._id === parseInt(req.params.userId))
+//   res.json(getId)
+// })
 
 //Update user - PUT PV
-app.put('/users/:userId', (req, res) => {
-  const found = users.some(user => user._id === parseInt(req.params.userId))
+// app.put('/users/:userId', (req, res) => {
+//   const found = users.some(user => user._id === parseInt(req.params.userId))
   
-  if (found) {
-      const updUsers = req.body
-      users.forEach(user => {
-        if(user._id === parseInt(req.params.userId)) {
-          user.name = updUsers.name
+//   if (found) {
+//       const updUsers = req.body
+//       users.forEach(user => {
+//         if(user._id === parseInt(req.params.userId)) {
+//           user.name = updUsers.name
 
-          res.json({msg : 'Member Updated', user})
-        }
-      })
-  } else {
-    res.status(400).json({msg: `No member with that id of ${req.params.id}`})
-  }
-})
+//           res.json({msg : 'Member Updated', user})
+//         }
+//       })
+//   } else {
+//     res.status(400).json({msg: `No member with that id of ${req.params.id}`})
+//   }
+// })
 
 //Delete User - Delete PV
-app.delete('/users/:userId', (req, res) => {
-  const found = users.some(user => user._id === parseInt(req.params.userId))
+// app.delete('/users/:userId', (req, res) => {
+//   const found = users.some(user => user._id === parseInt(req.params.userId))
 
-  if (found) {
-    res.json(users.filter(user => user._id !== parseInt(req.params.userId)))
-  } else {
-    res.status(400).json({msg : `No member with the id of ${req.params.id}`})
-  }
-})
+//   if (found) {
+//     res.json(users.filter(user => user._id !== parseInt(req.params.userId)))
+//   } else {
+//     res.status(400).json({msg : `No member with the id of ${req.params.id}`})
+//   }
+// })
 
 
 /* END - create routes here */
