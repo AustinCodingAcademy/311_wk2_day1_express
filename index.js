038c47d161b71439dee1795016fe523d2d8f197b
aaron-kane-w <aaron.kane.w@gmail.com>
2020-06-05 19:38:52 -0500

    assignment complete

--- index.js
+++ index.js
@@ -7,20 +7,23 @@ const { users } = require('./state')
 
 /* BEGIN - create routes here */
 
-// Get entire array
+// ## Part 1: Basic routes
+
+// GET /users
 app.get('/users', (req, res) => res.json(users))
 
-// Get single element from an array* work on this
-
+// GET /users/1
 app.get('/users/:_id', (req, res) => {
   res.json(users.find(user => {
     return +req.params._id === user._id
   }))
 })
 
-// create element in array (which requires body-parser middleware)
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+//* Body-Parser for Post & Put (npm i body-parser)
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+// POST /users
 
 app.post('/users', (req, res) => {
   const newUser = {
@@ -32,6 +35,27 @@ app.post('/users', (req, res) => {
   
   users.push(newUser)
   res.json(newUser)
+})
+
+// PUT /users/1
+app.put('/users/:_id', (req, res) => {
+  const updatedMember = req.body
+  users.forEach(user => {
+    if (user._id === +req.params._id ) {
+      user.name = updatedMember.name
+      res.json({ msg: 'User name updated', user})
+    }
+  })
+})
+
+// DELETE /users/1
+app.delete('/users/:_id', (req, res) => {
+  for (let i=0; i<users.length; i++) {
+    if (users[i]._id === +req.params._id) {
+      users[i].isActive = "False"
+      res.send('deleted')
+    }
+  }  
 })
 
 /* END - create routes here */
