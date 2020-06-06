47a1bc02a87f7895d038d85d352ed9cc9e3e3b57
MrGRivera <gabrielrivera2889@gmail.com>
2020-06-06 17:56:43 -0500

    part 1

--- index.js
+++ index.js
@@ -7,16 +7,38 @@ const { users } = require('./state')
 
 /* BEGIN - create routes here */
 app.get('/users', (req, res) => {
-  return res.json(users);
+  res.json(users);
 });
 
 app.get('/users/1', (req, res) => {
-  return res.json(users[0]);
+  res.json(users[0]);
 });
 
 app.post('/users', (req, res) => {
-  return res.send(res.json(users[4]));
+  let newUser = {
+    "_id": 6,
+    "name": "Han Solo",
+    "occupation": "Pilot",
+    "avatar": "https://www.starwars.com/databank/han-solo"
+  }
+
+  users.push(newUser);
+  res.json(users[users.length - 1]);
 });
 
-app.listen(port, () => 
+app.put('/users/1', (req, res) => {
+  users[0].name = 'Cale Dooper';
+  users[0].occupation = 'CIA Agent';
+  res.json(users[0]);
+});
+
+app.delete('/users/1', (req, res) => {
+  users.shift();
+  res.send('Deleted')
+});
+
+
+
+
+app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`))
