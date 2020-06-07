f96375b3aec2134517a76002da24dcf91ff7489a
MrGRivera <gabrielrivera2889@gmail.com>
2020-06-06 23:05:29 -0500

    my express project

--- index.js
+++ index.js
@@ -1,44 +1,86 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 4000;
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+const { users } = require("./state");
+let counter = users.length;
 
-const { users } = require('./state')
+app.use(express.json());
 
+/* PART 1 */
 /* BEGIN - create routes here */
-app.get('/users', (req, res) => {
+app.get("/users", (req, res) => {
   res.json(users);
 });
 
-app.get('/users/1', (req, res) => {
-  res.json(users[0]);
-});
+// app.get('/users/1', (req, res) => {
+//   res.json(users[0]);
+// });
 
-app.post('/users', (req, res) => {
+// app.post('/users', (req, res) => {
+//   let newUser = {
+//     "_id": 6,
+//     "name": "Han Solo",
+//     "occupation": "Pilot",
+//     "avatar": "https://www.starwars.com/databank/han-solo"
+//   }
+
+//   users.push(newUser);
+//   res.json(users[users.length - 1]);
+// });
+
+// app.put("/users/1", (req, res) => {
+//   users[0].name = "Cale Dooper";
+//   users[0].occupation = "CIA Agent";
+//   res.json(users[0]);
+// });
+
+// app.delete("/users/1", (req, res) => {
+//   users.shift();
+//   res.send("Deleted");
+// });
+
+/* PART 2 */
+
+app.post("/users", (req, res) => {
   let newUser = {
-    "_id": 6,
-    "name": "Han Solo",
-    "occupation": "Pilot",
-    "avatar": "https://www.starwars.com/databank/han-solo"
-  }
+    _id: counter,
+    name: req.body.name,
+    occupation: req.body.occupation,
+    avatar: req.body.avatar,
+  };
 
   users.push(newUser);
-  res.json(users[users.length - 1]);
+
+  counter++;
+
+  res.json(newUser);
 });
 
-app.put('/users/1', (req, res) => {
-  users[0].name = 'Cale Dooper';
-  users[0].occupation = 'CIA Agent';
-  res.json(users[0]);
+/* PART 3 */
+
+app.get("/users/:userId", (req, res) => {
+  res.json(users.filter((user) => user._id == req.params.userId));
 });
 
-app.delete('/users/1', (req, res) => {
-  users.shift();
-  res.send('Deleted')
+app.put("/users/:userId", (req, res) => {
+  users.forEach((user) => {
+    if (user._id == req.params.userId) {
+      user.name = "Cale Dooper";
+      user.occupation = "CIA Agent";
+      res.json(user);
+    }
+  });
 });
 
+app.delete("/users/:userId", (req, res) => {
+  users.forEach((user) => {
+    if (user._id == req.params.userId) {
+      user.isActive = "false";
+    }
+    res.send("Deleted");
+  });
+});
 
-
-
-app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
