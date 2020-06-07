f6659f570f2ab4a31483eb5c34431d3416980d3b
eh0ward <surpmusic@gmail.com>
2020-06-07 15:19:27 -0500

    311_wk2_d1_Express Homework Complete

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 4000;
 app.use(bodyParser.json());
 
 const { users } = require("./state");
-let counter = users.length + 1;
 
 /* BEGIN - create routes here */
 
@@ -22,11 +21,10 @@ app.get("/users", (req, res) => {
 
 // ---------------
 
-// GET /users/1:
-app.get("/users/1", (req, res) => {
-  // const userId = req.params.id;
-  res.json(users[0]);
-});
+// // GET /users/1:
+// app.get("/users/1", (req, res) => {
+//   res.json(users[0]);
+// });
 
 // ---------------
 
@@ -44,51 +42,99 @@ app.get("/users/1", (req, res) => {
 
 // ---------------
 
-// // PUT /users/1:
-app.put("/users/1", (req, res) => {
-  users[0].occupation = "Special Agent";
-  res.json(users[0]);
-  res.send({ msg: "Success, user updated!" });
-});
+// // // PUT /users/1:
+// app.put("/users/1", (req, res) => {
+//   users[0].occupation = "Special Agent";
+//   res.json(users[0]);
+//   res.send({ msg: "Success, user updated!" });
+// });
 
 // ---------------
 
 // DELETE /users/1
-app.delete("/users/1", (req, res) => {
-  users.shift();
-  res.send({ msg: "User Deleted!" });
-});
+// app.delete("/users/1", (req, res) => {
+//   users.shift();
+//   res.send({ msg: "User Deleted!" });
+// });
 
 // ---------------
 
 // Part 2. Body-parser module
 
 app.post("/users", (req, res) => {
-  let newUser = req.body;
+  let counter = users.length + 1;
+  const newUser = req.body;
   newUser._id = counter;
   users.push(newUser);
-  res.send({ msg: "New User Added!" });
-  res.json(req.body);
+  res.json({ msg: "New User Added!", user: newUser });
 });
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-// app.post("/users", (req, res) => {
-//   const newUser = {
-//     _id: req.body._id,
-//     name: req.body.name,
-//     occupation: req.body.occupation,
-//     avatar: req.body.avatar,
-//   };
-
-//   users.push(newUser);
-//   res.json(users[5]);
-// });
 
 // ---------------
 
 // Part 3. Use path variables
+
+// GET /users/1 => GET /users/:userId
+app.get("/users/:userId", (req, res) => {
+  const userFound = users.some(
+    (user) => user._id === parseInt(req.params.userId)
+  );
+
+  if (userFound) {
+    res.json(users.filter((user) => user._id === parseInt(req.params.userId)));
+  } else {
+    res
+      .status(400)
+      .json({ msg: `User with the ID of ${req.params.userId} not found!` });
+  }
+});
+
+// ---------------
+
+// PUT /users/1 => PUT /users/:userId
+app.put("/users/:userId", (req, res) => {
+  const userFound = users.some(
+    (user) => user._id === parseInt(req.params.userId)
+  );
+
+  if (userFound) {
+    const updUser = req.body;
+    users.forEach((user) => {
+      if (user._id === parseInt(req.params.userId)) {
+        user.name = updUser.name ? updUser.name : user.name;
+        user.occupation = updUser.occupation
+          ? updUser.occupation
+          : user.occupation;
+        user.avatar = updUser.avatar ? updUser.avatar : user.avatar;
+
+        res.json({ msg: "Success, user updated!", user });
+      }
+    });
+  } else {
+    res
+      .status(400)
+      .json({ msg: `User with the ID of ${req.params.userId} not found!` });
+  }
+});
+
+// ---------------
+
+// DELETE /users/1 => DELETE /users/:userId
+app.delete("/users/:userId", (req, res) => {
+  const userFound = users.some(
+    (user) => user._id === parseInt(req.params.userId)
+  );
+
+  if (userFound) {
+    res.json({
+      msg: "User Deleted!",
+      users: users.filter((user) => user._id === parseInt(req.params.userId)),
+    });
+  } else {
+    res
+      .status(400)
+      .json({ msg: `User with the ID of ${req.params.userId} not found!` });
+  }
+});
 
 /* END - create routes here */
 
