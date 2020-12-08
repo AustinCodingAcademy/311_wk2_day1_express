d4147fa08fdb0621dc525f3d7d1b5da59ed5d35e
ClaireSheek <clairesheek@gmail.com>
2020-12-07 22:48:25 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 
+const { response } = require('express')
 const express = require('express')
+const bodyParser = require(`body-parser`)
 const app = express()
 const port = process.env.PORT || 4000
 
 const { users } = require('./state')
+const counter = users.length
 
 /* BEGIN - create routes here */
+app.get('/users', (req, res) => {
+  return res.json(users)
+})
+app.get('/users/1', (req, res) => {
+  return res.json(users[0])
+})
 
+app.post('/users', (req, res) => {
+  let user =     
+  {
+    "_id": 6,
+    "name": "Claire Sheek",
+    "occupation": "Tap Dancer",
+    "avatar": "https://images.app.goo.gl/PrEpvpFnF1Cc61Jc7"
+  }
+  users.push(user)
+  return res.json(users)
+})
+
+app.put('/users/1', (req, res) => {
+  let currentUser = users[0]
+  currentUser.name = "John Doe"
+  return res.json(currentUser.name)
+})
+
+app.delete('/users/1', (req, res) => {
+  users.splice(0,1)
+  res.send("Deleted")
+  // return res.json(users)
+})
+
+app.post('/users', (req, res) => {
+  let user = req.body
+  
+  users.push(user)
+  return res.json(users)
+})
 
 /* END - create routes here */
 
