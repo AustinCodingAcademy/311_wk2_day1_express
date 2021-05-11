1ebc97a863e11ce1f91c28b6ee7e64f3132e3e84
RacMcT <raccmct@gmail.com>
2021-05-10 20:31:37 -0500

    part 1 completed

--- index.js
+++ index.js
@@ -7,6 +7,39 @@ const { users } = require('./state')
 
 /* BEGIN - create routes here */
 
+// PART ONE:
+
+//GET /users
+app.get('/users', function (req, res) {
+  res.json(users)
+});
+
+//GET /users/1
+app.get('/users/1', function (req, res){
+  res.json(users[0])
+});
+
+//POST /users
+app.post('/users', function (req, res) {
+  users.push({
+    '_id': users.length+1,
+    'name': 'Indiana Jones',
+    'occupation': 'Archaeologist',
+    'avatar': "https://upload.wikimedia.org/wikipedia/en/8/8c/Indiana_Jones_and_the_Last_Crusade.png"
+  })  
+  res.json(users[users.length-1])
+})
+
+//PUT /users/1
+app.put('/users/1', function (req, res) {
+  res.json(users[0].occupation = 'Nurse');
+})
+
+//DELETE /users/1
+app.delete('/users/1', function (req, res) {
+  res.send('user has been deleted')
+})
+
 
 /* END - create routes here */
 
