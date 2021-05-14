af90b118bd821ba03db15f9ecc6a450c2b4d96c0
Dominik Brame <idominikbrame@Dominiks-MacBook-Pro.local>
2021-05-14 11:51:15 -0500

    updated

--- index.js
+++ index.js
@@ -2,12 +2,32 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
-const { users } = require('./state')
+let { users } = require('./state')
 
 /* BEGIN - create routes here */
 
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+app.get(`/users/:id`, (req, res) => {
+  res.json(users[req.params.id - 1])
+})
 
+app.post('/users', (req, res) => {
+  const user_id = req.body._id;
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+
+  res.json(users.push({
+    '_id': user_id,
+    'name': name,
+    'occupation': occupation
+  }));
+})
 /* END - create routes here */
 
 app.listen(port, () => 
