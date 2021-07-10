49122741b183de4bf137be9bd44c3c5c63290bea
kiffykowboy <chris@Chriss-MacBook-Pro.local>
2021-07-10 12:19:06 -0500

    in progress

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ const port = process.env.PORT || 4000
 const { users } = require('./state')
 
 /* BEGIN - create routes here */
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+app.get('/users/1', (req, res) => {
+  res.json(users[0])
+})
 
+let newUser = {user:"Charlie Brown"};
+
+app.post('/users', function (req, res) {
+  res.json(newUser)
+})
+
+app.put('/users/1', function (req, res) {
+  res.json(users[0])
+})
+
+app.delete('/users/1', function (req, res) {
+  res.send("DELETED")
+})
 
 /* END - create routes here */
 
